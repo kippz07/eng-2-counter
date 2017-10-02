357f1c9d0b60c8c748ee2bbad8705a811d26feae
Jenny Johnson <kippz07@users.noreply.github.com>
2017-10-02 22:09:02 +0100

    added daily reset

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -5,14 +5,34 @@ class StaticController < ApplicationController
     end
 
     def update
-        person_params = params.require(:person).permit(:id, :count, :action)
+        person_params = params.require(:person).permit(:id, :action)
         id = person_params[:id]
         person = Person.find(id)
-        if (person_params[:action] == "swears")
-            person.update_attribute(:swears, person_params[:count].to_i + 1)
+        date = person.updated_at.to_s
+        last_date = date.split(" ")[0]
+        datetime_now = DateTime.now.to_s
+
+        # date_now = datetime_now.split("T")[0]
+        date_now = "2017/10/03"
+
+        if person_params[:action] == "swears"
+            if date_now != last_date 
+                person.update_attribute(:swears_today, 0)
+            else
+                person.update_attribute(:swears_today, person.swears_today + 1)
+                person.update_attribute(:swears, person.swears + 1)
+            end
+            
         else
-            person.update_attribute(:hacks, person_params[:count].to_i + 1)
+            if date_now != last_date 
+                person.update_attribute(:hacks_today, 0)
+            else
+                person.update_attribute(:hacks_today, person.hacks_today + 1)
+                person.update_attribute(:hacks, person.hacks + 1)
+            end
+            
         end
+
         redirect_to "/"
     end
 
